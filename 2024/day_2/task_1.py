1b7cdb35eddd58082401a36588cd581f5e04bd86
Fran Cudina <cudina.fran@gmail.com>
2024-12-02 23:02:02 +0100

    day2

---
+++ 2024/day_2/task_1.py
@@ -0,0 +1,27 @@
+def read() -> []:
+    with open('input.txt') as f:
+        return [[int(num) for num in l.strip().split()] for l in f.readlines()]
+
+
+def scan(line: []) -> int:
+    prev = None
+    for i in range(len(line) - 1):
+
+        d = line[i + 1] - line[i]
+        if not (1 <= abs(d) <= 3):
+            return 0
+
+        curr = True if d > 0 else False
+
+        if prev is None:
+            prev = curr
+        elif prev != curr:
+            return 0
+
+    return 1
+
+
+if __name__ == '__main__':
+
+    rez = sum(scan(l) for l in read())
+    print(f'\nResult: {rez}')
---
+++ 2024/day_2/task_2.py
@@ -0,0 +1,16 @@
+from task_1 import read, scan
+
+
+rez = 0
+for l in read():
+
+    if scan(l):
+        rez += 1
+        continue
+
+    for i in range(len(l)):
+        if scan([e for j, e in enumerate(l) if i != j]):
+            rez += 1
+            break
+
+print(f'\nResult: {rez}')
