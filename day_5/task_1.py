130269c41125c5816a8cb45c9441869e21049713
Fran Cudina <cudina.fran@gmail.com>
2024-12-05 13:45:27 +0100

    day 5

---
+++ day_5/task_1.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+with open('input.txt') as f:
+    lines = [l.strip() for l in f.readlines()]
+
+rules = defaultdict(list)
+[rules[l.split('|')[0]].append(l.split('|')[1]) for l in lines if '|' in l]
+
+
+def _check(u: []) -> int:
+    found = any([any(next_p for next_p in rules[u[i]] if next_p in u[:i]) for i in range(len(u) - 1, -1, -1)])
+    return int(u[int(len(u) / 2)]) if not found else 0
+
+
+rez = 0
+for update in [l.split(',') for l in lines if '|' not in l and len(l) != 0]:
+    rez += _check(update)
+
+print(f'Result: {rez}')
---
+++ day_5/task_2.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+with open('input.txt') as f:
+    lines = [l.strip() for l in f.readlines()]
+
+rules = defaultdict(list)
+[rules[l.split('|')[0]].append(l.split('|')[1]) for l in lines if '|' in l]
+
+
+def _greater(c: str, n: str) -> bool:
+    return n not in rules[c]
+
+
+def _check(u: []) -> int:
+    valid = list(u)
+    for i in range(len(u) - 1):
+        swapped = False
+        for j in range(len(u) - i - 1):
+            if _greater(valid[j], valid[j + 1]):
+                valid[j], valid[j + 1] = valid[j + 1], valid[j]
+                swapped = True
+        if not swapped:
+            break
+    return int(valid[int(len(u) / 2)]) if valid != u else 0
+
+
+rez = 0
+for update in [l.split(',') for l in lines if '|' not in l and len(l) != 0]:
+    rez += _check(update)
+
+print(f'Result: {rez}')
