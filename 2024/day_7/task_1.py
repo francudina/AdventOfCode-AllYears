506a87fd2ceb1ceb4cae0b24cd6ce00a3f80cb32
Fran Cudina <cudina.fran@gmail.com>
2024-12-07 09:52:15 +0100

    day7

---
+++ 2024/day_7/task_1.py
@@ -0,0 +1,40 @@
+from itertools import product
+
+
+def _read() -> []:
+    with open('input.txt') as f:
+        return [l.strip().split(':') for l in f.readlines()]
+
+
+def combinations(n: int, values: []) -> []:
+    return [list(c) for c in product(values, repeat=n)]
+
+
+def combine(operators: [], pairs: []) -> int:
+    r = int(pairs[0])
+    for i in range(len(operators)):
+        if operators[i] == '+':
+            r += int(pairs[i+1])
+        elif operators[i] == '*':
+            r *= int(pairs[i+1])
+        elif operators[i] == '||':
+            r = int(str(r) + pairs[i+1])
+    return r
+
+
+def run(operators: []):
+    rez = 0
+    for l in _read():
+        left, right = int(l[0]), l[1].strip().split()
+
+        for o in combinations(n=len(right) - 1, values=operators):
+            comb: int = combine(operators=o, pairs=right)
+            if left == comb:
+                rez += left
+                break
+
+    print(f'Result: {rez}')
+
+
+if __name__ == '__main__':
+    run(operators=['+', '*'])
---
+++ 2024/day_7/task_2.py
@@ -0,0 +1,3 @@
+from task_1 import run
+
+run(operators=['+', '*',  '||'])
